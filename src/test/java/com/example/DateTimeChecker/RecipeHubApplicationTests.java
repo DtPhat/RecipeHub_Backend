2c2939147cd4bb5e644d6885d5a6ccb0032d53bf
DtPhat <dtphatkh@gmail.com>
2024-05-23 17:42:18 +0700

    update application properties and test file

--- src/test/java/com/example/DateTimeChecker/RecipeHubApplicationTests.java
+++ src/test/java/com/example/DateTimeChecker/RecipeHubApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.DateTimeChecker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.RecipeHub.RecipeHubApplication;
+
+@SpringBootTest(classes = RecipeHubApplication.class)
 class RecipeHubApplicationTests {
 
 	@Test
